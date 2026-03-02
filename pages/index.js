5aeddf270918d6507ca7ac95454252c66adfecd0
Dave Manning <dave@davemanning.com>
2026-03-02 00:32:49 -0500

    Add Home component for initial React app setup

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Success!</h1>
+      <p>Your React app is now running on Webflow Cloud at /app.</p>
+    </div>
+  );
+}
