eada0023c2b7bce2b2c652915f03762bf00c3ca3
Dave Manning <dave@davemanning.com>
2026-03-16 14:42:49 -0400

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,20 +1,24 @@
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-  edgeExternals: ["node:crypto"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-};
-
-export default config;
+export default { 
+  default: { 
+    override: { 
+      wrapper: "cloudflare-node", 
+      converter: "edge", 
+      proxyExternalRequest: "fetch", 
+      incrementalCache: "dummy", 
+      tagCache: "dummy", 
+      queue: "dummy"
+    }, 
+  }, 
+  edgeExternals: ["node:crypto"], 
+  middleware: { 
+    external: true, 
+    override: { 
+      wrapper: "cloudflare-edge", 
+      converter: "edge", 
+      proxyExternalRequest: "fetch", 
+      incrementalCache: "dummy", 
+      tagCache: "dummy", 
+      queue: "dummy"
+    }, 
+  }, 
+}
